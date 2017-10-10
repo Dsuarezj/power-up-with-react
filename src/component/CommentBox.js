c68a0949412213bf05d829233d821701722a0317
Denisse Suarez <dsuarez@thoughtworks.com>
2017-10-09 21:24:26 -0500

    Making Comment Box handdle dynamic props, receiving comments from a json

--- src/component/CommentBox.js
+++ src/component/CommentBox.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React from "react";
 import Comment from "./Comment";
 
 
-class CommentBox extends React.Component{
-    render(){
-        return(
-          <div className="comment-box">
-              <h3>Comments</h3>
-              <h4 className="comment-count"> 2 comments</h4>
-              <div className="comment-list">
-                  <Comment author="Anonimo 1" body="primer comentario"/>
-                  <Comment author="Anonimo 2" body="segundo comentario"/>
-              </div>
-          </div>
+class CommentBox extends React.Component {
+    render() {
+        const comments = this._getComments();
+        const commentsTittle = this._getCommentsTittle(comments.length);
+        return (
+            <div className="comment-box">
+                <h3>Comments</h3>
+                <h4 className="comment-count"> {commentsTittle}</h4>
+                <div className="comment-list">
+                    {comments}
+                </div>
+            </div>
         );
     }
+
+    _getComments() {
+        const commentList = [
+            {id: 1, author: 'Anonimo 101', body: 'primer comentario'},
+            {id: 2, author: 'Anonimo 202', body: 'segundo comentario'}
+        ];
+        return commentList.map((comment) => {
+            return <Comment author={comment.author} body={comment.body} key={comment.id}/> //Key helps react to keep track which element is which in the loop, is a good practice to pass a unique number for this key
+        });
+    }
+
+    _getCommentsTittle(commentCount) {
+        if (commentCount === 0) {
+            return "No comments yet.";
+        } else if (commentCount === 1) {
+            return "1 comment";
+        } else {
+            return `${commentCount} comments` ;
+        }
+    }
+
 }
 
 export default CommentBox;
